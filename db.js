e05d1fda17b2cfa0ac150bc23970526456639c3e
cmurilo1 <contatocodegeniuss@gmail.com>
2026-06-15 19:27:02 -0300

    Correcao no arquivo db.js

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql2');
-//require('dotenv').config();
 
 // Configura o grupo de conexões com o MySQL
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '', // Coloque sua senha do banco se tiver
-    database: process.env.DB_NAME || 'app_flashcards',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 // Transforma em Promises para usarmos async/await
