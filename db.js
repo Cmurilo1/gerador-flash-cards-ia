7150a0e14326901758b8bb18b1bbee302b8889bc
cmurilo1 <contatocodegeniuss@gmail.com>
2026-06-16 23:32:14 -0300

    Corrigindo o db.js

--- db.js
+++ db.js
@@ -1,18 +1,14 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-// Configura o grupo de conexões com o MySQL
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-// Transforma em Promises para usarmos async/await
-const db = pool.promise();
-
-module.exports = db;
+module.exports = pool;
